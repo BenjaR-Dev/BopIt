08437bee05d79d4db2c64ed89e6635d0ed9ece4f
BenjaR-Dev <benignrodriguez@gmail.com>
2023-11-01 00:44:45 -0300

    Laboratorio 2 - implementada la música y sonidos y la detección del gesto onFling

--- README.txt
+++ README.txt
@@ -1,14 +1,14 @@
-Aplicación: Bop It Mobile (1ra versión)
+Aplicación: Bop It Mobile (2da versión)
 Desarrolado por: Benjamín Rodríguez
 
 
 En este juego deberás realizar la acción que se te indique por pantalla lo antes posible, ¡antes de que se acabe el tiempo!, deberás ser rápido o de lo contrario perderás.
 
+En la segunda versión de esta aplicación se implementó sonido de victoria y derrota además de la música de fondo en la pantalla de juego. Por otro lado también se implementó la detección del gesto de deslizar (onFling) y hacia qué dirección se está realizando.
 
 
 Link para descargar APK:
-https://drive.google.com/file/d/1RTllYm-v_Q4hqmN-tI6W-hR1tp1rsY23/view?usp=sharing
-
+https://drive.google.com/file/d/18Qdf18TB6jrOYMStqGvK8kRcsKO56JO1/view?usp=sharing
 
 Link para ver video presentación:
-https://youtu.be/_v96JOND8ZA
+https://youtu.be/bzqtQ8HaO8I
--- app/src/main/java/cl/brodriguez/bopit/PantallaJuegoActivity.kt
+++ app/src/main/java/cl/brodriguez/bopit/PantallaJuegoActivity.kt
@@ -1,11 +1,89 @@
 package cl.brodriguez.bopit
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
+import kotlin.math.abs
 
 class PantallaJuegoActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var mediaPlayer2: MediaPlayer
+    private lateinit var mediaPlayer3: MediaPlayer
+
+
+    private lateinit var gestureDetector: GestureDetector
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_juego)
+
+        gestureDetector = GestureDetector(this, MyGestureListener())
+
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.win)
+        mediaPlayer2 = MediaPlayer.create(this, R.raw.defeat)
+        mediaPlayer3 = MediaPlayer.create(this, R.raw.background)
+
+        mediaPlayer3.isLooping = true
+        mediaPlayer3.start()
     }
+
+    fun sonidoVictoria(view: View){
+        mediaPlayer.start()
+    }
+
+    fun sonidoDerrota(view: View){
+        mediaPlayer2.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+        mediaPlayer2.release()
+        mediaPlayer3.release()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gestureDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    inner class MyGestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            //Manejo del evento
+            if(abs(e1.x - e2.x) > abs(e1.y - e2.y)){
+                if (e1.x < e2.x) {
+                    //hacia la derecha
+                    mostrarMensajeToast("Deslizamiento hacia la derecha")
+                } else {
+                    //hacia la izquierda
+                    mostrarMensajeToast("Deslizamiento hacia la izquierda")
+                }
+            }else{
+                if (e1.y > e2.y) {
+                    //hacia arriba
+                    mostrarMensajeToast("Deslizamiento hacia arriba")
+                } else {
+                    //hacia abajo
+                    mostrarMensajeToast("Deslizamiento hacia abajo")
+                }
+            }
+
+            return true
+        }
+    }
+
+    private fun mostrarMensajeToast(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
